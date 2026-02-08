8544aedf65e237bcbaa4d6bafd884a2ab5a33db1
Abebaye Agumasie <yasminagumasie@gmail.com>
2026-02-07 23:07:40 -0900

    Add feedback and strength analysis

--- password_checker.py
+++ password_checker.py
@@ -1,19 +1,42 @@
 password = input ("Enter your password: ")
 score = 0
+feedback= []
+
 if len(password) >= 8:
     score += 1
+else:
+    feedback.append("Password should be at least 8 characters long")
+
 if any(char.isupper() for char in password):
     score += 1
+else:
+    feedback.append("Add atleast one upper letter")
+
 if any(char.islower() for char in password):
     score += 1
+else:
+    feedback.append("Add atleast one lowercase letter")
+
 if any (char.isdigit() for char in password):
     score += 1
+else:
+    feedback.append("Add atleast one number")
+
 if any(not char.isalnum() for char in password):
     score += 1
+else:
+    feedback.append("Add atleast one special character")
+
+print("\nPassword Strength:")
 
 if score <= 2:
-    print("Password Strenght: WEAK")
-elif score ==3 or score ==4:
-    print("Password Strength: MEDIUM")
+    print("❌ WEAK")
+elif score <= 4:
+    print("⚠️ MEDIUM")
 else:
-    print("Password Strength: STRONG")
+    print("✅ STRONG")
+
+if feedback:
+    print("\n Suggestions:")
+    for item in feedback:
+        print("-", item)
